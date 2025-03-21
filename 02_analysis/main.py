ee451a18f49ed87187d09670d8e1f2744a8042bd
masamasace <shigamasataka@gmail.com>
2025-03-21 12:48:24 +0900

    Update cv374 subproject reference and modify main.py to remove unused DBLHandler code

--- 02_analysis/main.py
+++ 02_analysis/main.py
@@ -3,5 +3,8 @@ from cv374 import T3WHandler, Win32Handler, DBLHandler, WINHandler
 
 # data = DataLoader("01_data")
 
-temp_dbl_data = DBLHandler(r"01_data\01_群馬高専\Trillium\200327_20250210111729.dbl")
+# temp_dbl_data = DBLHandler(r"01_data\01_群馬高専\Trillium\200327_20250210111729.dbl")
+# print(temp_dbl_data.header_fixed)
+# print(temp_dbl_data.header_variable)
+# print(temp_dbl_data.data)
 temp_win_data = WINHandler(r"01_data/01_群馬高専/D013/25021011.36")
