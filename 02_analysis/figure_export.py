bfed74c39a24ee1a98651d63ef23733731813398
masamasace <shigamasataka@gmail.com>
2025-03-24 09:44:10 +0900

    Update cv374 subproject reference and remove main.py; add data_combiner and figure_export scripts

---
+++ 02_analysis/figure_export.py
@@ -0,0 +1,8 @@
+from cv374 import DataLoader, DataCombiner
+from pathlib import Path
+
+data_dir = r"01_data"
+# data_dir = r'01_data\01_群馬高専\D013'
+data = DataLoader(data_dir)
+combined_data = DataCombiner(data)
+
--- 02_analysis/main.py
+++
@@ -1,7 +0,0 @@
-from cv374 import DataLoader, DataCombiner
-
-data_dir = r"01_data"
-# data_dir = r'01_data\01_群馬高専\D013'
-data = DataLoader(data_dir)
-combined_data = DataCombiner(data)
-
